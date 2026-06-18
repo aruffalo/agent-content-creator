b2ff559290dc481b79bfdd6d7abed886308c4984
aruffalo <varicoloredvalley@gmail.com>
2026-06-18 07:10:20 -0700

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
     return res.status(405).json({ error: "Method not allowed" });
   }
 
-  const apiKey = process.env.TCRA_2;;
+  const apiKey = process.env.a1b2c3999;
   if (!apiKey) {
     return res.status(500).json({ error: "Server configuration error — API key missing." });
   }
